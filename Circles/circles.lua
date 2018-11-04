76cf5a06dec18f300406a211b50929feb92c33cd
Taylor <tjameswitt@gmail.com>
2018-11-04 15:08:28 -0500

    Add files via upload

--- Circles/circles.lua
+++ Circles/circles.lua
@@ -16,14 +16,28 @@ require('chat')
 res = require('resources')
 packets = require('packets')
 
+startDistance = 8
 continue = 0
+zoneID = 0
 
---ru'lude gardens
-waypoints = T{
-	[1] = {x = 30, y = -6},
-	[2] = {x = -27, y = -6},
-	[4] = {x = -27, y = -73},
-	[3] = {x = 30, y = -73}
+wp = T{
+	[243] = T{ --Ru'Lude Gardens
+		[1] = {x = 30, y = -6},
+		[2] = {x = -27, y = -6},
+		[4] = {x = -27, y = -73},
+		[3] = {x = 30, y = -73}
+	},
+	[249] = T{ --Mhaura
+		[1] = {x = -10, y = 67},
+		[2] = {x = -16, y = -16},
+		[3] = {x = 46, y = 74},
+		[4] = {x = 44, y = 48},
+		[5] = {x = 44, y = 48},
+		[6] = {x = -21, y = 44},
+		[7] = {x = -29, y = 35},
+		[8] = {x = -39, y = 25},
+		[9] = {x = -36, y = 54}
+	}
 }
 
 windower.register_event('addon command', function(...)
@@ -31,11 +45,15 @@ windower.register_event('addon command', function(...)
     local cmd = args[1]
 	if cmd then 
 		if cmd:lower() == 'start' then
-			continue = 1
-			coroutine.sleep(1)
+			if ValidateLocation() == 1 then
+				continue = 1
+				coroutine.sleep(1)
+			end
 		elseif cmd:lower() == 'stop' then
 			windower.add_to_chat(2,'Stopping.')
 			continue = 0
+		elseif cmd:lower() == 'test' then
+			ValidateLocation()
 		end
 	end
 	
@@ -48,11 +66,12 @@ function RunCircles()
 	--start near waypoints[1]
 	--local player = windower.ffxi.get_mob_by_target('me')
 	--local vecPlayer = UpdatePlayerPosition()
+	local zone = windower.ffxi.get_info()['zone']
 	local vecPlayer
 	local vecWaypoint
 	local dist
 	
-	for i,v in pairs(waypoints) do
+	for i,v in pairs(wp[zone]) do
 		vecWaypoint = {x = v.x, y = v.y}
 		dist = 9999
 		while (dist > 4) and (continue == 1) do
@@ -65,6 +84,26 @@ function RunCircles()
 				
 	end
 	
+end
+
+function ValidateLocation()
+	local zone = windower.ffxi.get_info()['zone']
+	local player = windower.ffxi.get_mob_by_target('me')
+	local vecStart
+	
+	if wp[zone] ~= nil then
+		vecStart = wp[zone][1]
+		if GetDistance(player, vecStart) < startDistance then
+			windower.add_to_chat(2,'Validated')
+			return 1
+		else
+			windower.add_to_chat(2,'Too far from starting point -- move closer to '..vecStart.x..', '..vecStart.y)
+			return 0
+		end
+	else
+		windower.add_to_chat(2,'Current zone not currently implemented.')
+		return 0
+	end
 end
 
 function GetDistance(player, location)
