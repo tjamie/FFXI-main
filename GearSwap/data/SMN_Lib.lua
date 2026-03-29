f0fff68ff48f5c30977899f80201384cc4603732
Taylor <17496150+myrcheburgers@users.noreply.github.com>
2026-03-29 13:43:44 -0400

    move speed update

--- GearSwap/data/SMN_Lib.lua
+++ GearSwap/data/SMN_Lib.lua
@@ -1145,29 +1145,29 @@ if player and player.index and windower.ffxi.get_mob_by_index(player.index) then
     end
     moving = false
 
-    windower.raw_register_event('prerender',function()
-        mov.counter = mov.counter + 1;
-        if mov.counter>30 then
-            local pl = windower.ffxi.get_mob_by_index(player.index)
-            if pl and pl.x and mov.x then
-                local movement = math.sqrt( (pl.x-mov.x)^2 + (pl.y-mov.y)^2 + (pl.z-mov.z)^2 ) > 0.1
-                if movement and not moving then
-                    send_command('gs c toggle runspeed')
-                    moving = true
-                elseif not movement and moving then
-                    send_command('gs c toggle runspeed')
-                    moving = false
-                end
-            end
+    -- windower.raw_register_event('prerender',function()
+    --     mov.counter = mov.counter + 1;
+    --     if mov.counter>30 then
+    --         local pl = windower.ffxi.get_mob_by_index(player.index)
+    --         if pl and pl.x and mov.x then
+    --             local movement = math.sqrt( (pl.x-mov.x)^2 + (pl.y-mov.y)^2 + (pl.z-mov.z)^2 ) > 0.1
+    --             if movement and not moving then
+    --                 send_command('gs c toggle runspeed')
+    --                 moving = true
+    --             elseif not movement and moving then
+    --                 send_command('gs c toggle runspeed')
+    --                 moving = false
+    --             end
+    --         end
 
-            if pl and pl.x then
-                mov.x = pl.x
-                mov.y = pl.y
-                mov.z = pl.z
-            end
-            mov.counter = 0
-        end
-    end)
+    --         if pl and pl.x then
+    --             mov.x = pl.x
+    --             mov.y = pl.y
+    --             mov.z = pl.z
+    --         end
+    --         mov.counter = 0
+    --     end
+    -- end)
 
     windower.register_event('prerender', function()
         --Items we want to check every second
